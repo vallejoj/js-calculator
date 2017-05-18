d2b030324a7d58f0ea68a3274dda7456b0e82818
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-17 21:35:26 -1000

    Moving forward.

--- calculator.js
+++ calculator.js
@@ -6,52 +6,59 @@
  * @return {object} `calculator` object that can be used
  */
 function calculatorModule() {
-  var memory;
+  var memory=0;
   var total= 0;
   var calculator={};
+
+  function load(x){
+    total=x;
+    return total;
+  }
+  function getTotal(){
+return total
+  }
+  function add(x){
+    var num= x
+    total+=x
+return total
+  }
+  function subtract(x){
+    var num= x
+    total-=x
+return total
+  }
+  function multiply(x){
+    var num= x
+    total*=x
+return total
+  }
+  function divide(x){
+    var num= x
+    total/=x
+return total
+  }
+  function recallMemory(){
+    return memory;
+  }
+  function saveMemory(){
+   memory=total;
+   return memory;
+  }
+  return {
+    load:load,
+    getTotal:getTotal,
+    add:add,
+    subtract:subtract,
+    multiply:multiply,
+    divide:divide,
+    recallMemory:recallMemory,
+    saveMemory:saveMemory,
+  }
+
 }
 
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
 
 
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
-
-
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
-
-
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
 
 
   /**
